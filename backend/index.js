56620ec68662d6d246f9b7dfb4f6c9bd6b53e104
Archit301 <tambiarchit@gmail.com>
2024-09-11 03:41:08 +0530

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,6 @@ const app = express();
 
 app.use(cors());
 
-// Serve static files from the frontend build directory
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-// Serve index.html for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-});
 
 const PORT =  7000;
 const MONGO =  "mongodb+srv://archit:1234@cluster0.gb71m.mongodb.net/mydatabase";
@@ -25,7 +18,15 @@ mongoose.connect(MONGO)
   .catch(err => {
     console.error("Database connection error:", err);
   });
-
+  const __dirname = path.resolve()
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
