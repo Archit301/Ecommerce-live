b357808beecc4cfb3fde60b4cdcc51140d4a49a3
Archit301 <tambiarchit@gmail.com>
2024-09-11 02:42:08 +0530

    modified:   frontend/src/App.jsx 	modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      // Proxy API requests to the backend server
+      '/backend': {
+        target: 'http://localhost:7000', // Replace with your backend URL
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
