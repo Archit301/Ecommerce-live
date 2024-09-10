5f53c2f0e03791b0311f59a0ab9a5ac07afe006f
Archit301 <tambiarchit@gmail.com>
2024-09-11 02:29:29 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+
+const app=express()
+// const server=http.createServer(app)
+
+app.use(cors())
+
+
+const PORT=7000;
+
+
+app.listen(PORT,()=>{
+         console.log(`Server is running on port ${PORT}`);
+     });
